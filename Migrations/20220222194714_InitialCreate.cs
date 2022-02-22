cd6cc4096823fbac3144112d5f9548a5ebcb3b49
Rasmus Fogelberg <fogelbergrasmus@gmail.com>
2022-02-22 20:52:10 +0100

    Add migrations

---
+++ Migrations/20220222194714_InitialCreate.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DiscosaurusApi.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Albums",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ArtistId = table.Column<int>(type: "int", nullable: false),
+                    GenreId = table.Column<int>(type: "int", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Duration = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Albums", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Artist",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Artist", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Genre",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genre", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Albums",
+                columns: new[] { "Id", "ArtistId", "Duration", "GenreId", "Title" },
+                values: new object[,]
+                {
+                    { 1, 1, 507, 1, "Thunderstruck" },
+                    { 2, 2, 731, 2, "Balls to the Wall" },
+                    { 3, 3, 441, 3, "The Number of the Beast" },
+                    { 4, 4, 955, 1, "We Like It Here" },
+                    { 5, 5, 456, 3, "Volume Rock" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Artist",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "AC/DC" },
+                    { 2, "Accept" },
+                    { 3, "Iron Maiden" },
+                    { 4, "Snarky Puppy" },
+                    { 5, "Valley Of The Sun" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Genre",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Rock" },
+                    { 2, "Jazz" },
+                    { 3, "Metal" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Albums");
+
+            migrationBuilder.DropTable(
+                name: "Artist");
+
+            migrationBuilder.DropTable(
+                name: "Genre");
+        }
+    }
+}
